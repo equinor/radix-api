31243499cb354bf0eac7cc7837281395f05704a3
ingeknudsen <iknu@equinor.com>
2018-10-09 09:33:44 +0200

    Attempt to make api work in cluster again

--- api/platform/platform_handler_test.go
+++ api/platform/platform_handler_test.go
@@ -3,17 +3,18 @@ package platform
 import (
 	"testing"
 
+	"github.com/statoil/radix-operator/pkg/client/clientset/versioned/fake"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetCloneURLRepo(t *testing.T) {
+func TestGetCloneURLRepo_ValidRepo_CreatesValidClone(t *testing.T) {
 	expected := "git@github.com:Equinor/my-app.git"
 	actual, _ := getCloneURLFromRepo("https://github.com/Equinor/my-app")
 
 	assert.Equal(t, actual, expected, "getCloneURLFromRepo - not equal")
 }
 
-func TestFilterOnSSHRepo(t *testing.T) {
+func TestFilterOnSSHRepo_WhenSet_Filter(t *testing.T) {
 	builder := NewBuilder()
 	rr, _ := builder.withRepository("https://github.com/Equinor/my-app").BuildRR()
 
@@ -28,4 +29,34 @@ func TestFilterOnSSHRepo(t *testing.T) {
 	expected = false
 	actual = filterOnSSHRepo(rr, " ")
 	assert.Equal(t, actual, expected, "filterOnSSHRepo - expected to not be filtered as filter is not provided")
+}
+
+func TestCreateApplication_WhenRepoAndDeployKeyNotSet_GenerateDeployKey(t *testing.T) {
+	builder := NewBuilder()
+	builder.withName("Some Name")
+
+	radixclient := fake.NewSimpleClientset()
+	registration, _ := HandleCreateRegistation(radixclient, builder.BuildRegistration())
+
+	// Restart
+	radixclient = fake.NewSimpleClientset()
+	expected := ""
+	actual := registration.PublicKey
+	assert.Equal(t, actual, expected, "HandleCreateRegistation - when repo is missing, do not generate deploy key")
+
+	// Restart
+	radixclient = fake.NewSimpleClientset()
+	builder.withRepository("Some repo string")
+	registration, _ = HandleCreateRegistation(radixclient, builder.BuildRegistration())
+
+	assert.NotEmpty(t, registration.PublicKey, "HandleCreateRegistation - when repo is provided, and deploy key is not, generate deploy key")
+
+	// Restart
+	radixclient = fake.NewSimpleClientset()
+	builder.withPublicKey("Some public key")
+	registration, _ = HandleCreateRegistation(radixclient, builder.BuildRegistration())
+	expected = "Some public key"
+	actual = registration.PublicKey
+
+	assert.Equal(t, actual, expected, "HandleCreateRegistation - when repo is provided, as well as deploy key, do not generate deploy key")
 }
