f026edd42effc48219299f3229e37b411700866c
ingeknudsen <iknu@equinor.com>
2018-10-15 13:33:38 +0200

    Added testing + prepare to merge rd with ra

--- api/deployment/deployment_handler_test.go
+++ api/deployment/deployment_handler_test.go
@@ -4,7 +4,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/statoil/radix-operator/pkg/apis/radix/v1"
+	"github.com/statoil/radix-api/api/utils"
+
 	radix "github.com/statoil/radix-operator/pkg/client/clientset/versioned/fake"
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
@@ -16,12 +17,12 @@ func TestGetDeployments_Filter_FilterIsApplied(t *testing.T) {
 	kubeclient := kubernetes.NewSimpleClientset()
 	radixclient := radix.NewSimpleClientset()
 
-	save(kubeclient, radixclient, NewDeploymentBuilder().withAppName("anyapp1").withEnvironment("prod").withImageTag("abcdef"))
+	apply(kubeclient, radixclient, NewDeploymentBuilder().withAppName("anyapp1").withEnvironment("prod").withImageTag("abcdef"))
 
 	// Ensure the second image is considered the latest version
-	save(kubeclient, radixclient, NewDeploymentBuilder().withAppName("anyapp2").withEnvironment("dev").withImageTag("ghijklm").withTime(time.Now()))
-	save(kubeclient, radixclient, NewDeploymentBuilder().withAppName("anyapp2").withEnvironment("dev").withImageTag("nopqrst").withTime(time.Now().AddDate(0, 0, 1)))
-	save(kubeclient, radixclient, NewDeploymentBuilder().withAppName("anyapp2").withEnvironment("prod").withImageTag("uvwxyza"))
+	apply(kubeclient, radixclient, NewDeploymentBuilder().withAppName("anyapp2").withEnvironment("dev").withImageTag("ghijklm").withCreated(time.Now()))
+	apply(kubeclient, radixclient, NewDeploymentBuilder().withAppName("anyapp2").withEnvironment("dev").withImageTag("nopqrst").withCreated(time.Now().AddDate(0, 0, 1)))
+	apply(kubeclient, radixclient, NewDeploymentBuilder().withAppName("anyapp2").withEnvironment("prod").withImageTag("uvwxyza"))
 
 	deployments, _ := HandleGetDeployments(radixclient, "", "", false)
 	assert.Equal(t, 4, len(deployments), "GetDeployments - no filter should list all")
@@ -52,34 +53,77 @@ func TestPromote_ErrorScenarios_ErrorIsReturned(t *testing.T) {
 
 	_, err := HandlePromoteEnvironment(kubeclient, radixclient, "", PromotionParameters{FromEnvironment: "dev", ImageTag: "abcdef", ToEnvironment: "prod"})
 	assert.Error(t, err, "HandlePromoteEnvironment - Cannot promote empty app")
+	assert.Equal(t, "App name is required", (err.(*utils.Error)).Message, "HandlePromoteEnvironment - Unexpected message")
 
 	_, err = HandlePromoteEnvironment(kubeclient, radixclient, "noapp", PromotionParameters{FromEnvironment: "dev", ImageTag: "abcdef", ToEnvironment: "prod"})
 	assert.Error(t, err, "HandlePromoteEnvironment - Cannot promote non-existing app")
 
-	save(kubeclient, radixclient, NewDeploymentBuilder().withAppName("anyapp").withEnvironment("prod").withImageTag("abcdef"))
+	apply(kubeclient, radixclient, NewDeploymentBuilder().withAppName("anyapp").withEnvironment("prod").withImageTag("abcdef"))
 	_, err = HandlePromoteEnvironment(kubeclient, radixclient, "anyapp", PromotionParameters{FromEnvironment: "dev", ImageTag: "abcdef", ToEnvironment: "prod"})
 	assert.Error(t, err, "HandlePromoteEnvironment - Cannot promote from non-existing environment")
+	assert.Equal(t, "Non existing from environment", (err.(*utils.Error)).Message, "HandlePromoteEnvironment - Unexpected message")
 
-	save(kubeclient, radixclient, NewDeploymentBuilder().withAppName("anyapp1").withEnvironment("dev").withImageTag("abcdef"))
+	apply(kubeclient, radixclient, NewDeploymentBuilder().withAppName("anyapp1").withEnvironment("dev").withImageTag("abcdef"))
 	_, err = HandlePromoteEnvironment(kubeclient, radixclient, "anyapp1", PromotionParameters{FromEnvironment: "dev", ImageTag: "abcdef", ToEnvironment: "prod"})
 	assert.Error(t, err, "HandlePromoteEnvironment - Cannot promote to non-existing environment")
+	assert.Equal(t, "Non existing to environment", (err.(*utils.Error)).Message, "HandlePromoteEnvironment - Unexpected message")
 
-	save(kubeclient, radixclient, NewDeploymentBuilder().withAppName("anyapp2").withEnvironment("dev").withImageTag("abcdef"))
-	save(kubeclient, radixclient, NewDeploymentBuilder().withAppName("anyapp2").withEnvironment("prod").withImageTag("ghijklm"))
+	apply(kubeclient, radixclient, NewDeploymentBuilder().withAppName("anyapp2").withEnvironment("dev").withImageTag("abcdef"))
+	apply(kubeclient, radixclient, NewDeploymentBuilder().withAppName("anyapp2").withEnvironment("prod").withImageTag("ghijklm"))
 	_, err = HandlePromoteEnvironment(kubeclient, radixclient, "anyapp2", PromotionParameters{FromEnvironment: "dev", ImageTag: "nopqrst", ToEnvironment: "prod"})
 	assert.Error(t, err, "HandlePromoteEnvironment - Cannot promote non-existing image")
+	assert.Equal(t, "Non existing image", (err.(*utils.Error)).Message, "HandlePromoteEnvironment - Unexpected message")
 
-	save(kubeclient, radixclient, NewDeploymentBuilder().withAppName("anyapp3").withEnvironment("dev").withImageTag("abcdef"))
-	save(kubeclient, radixclient, NewDeploymentBuilder().withAppName("anyapp3").withEnvironment("prod").withImageTag("abcdef"))
+	apply(kubeclient, radixclient, NewDeploymentBuilder().withAppName("anyapp3").withEnvironment("dev").withImageTag("abcdef"))
+	apply(kubeclient, radixclient, NewDeploymentBuilder().withAppName("anyapp3").withEnvironment("prod").withImageTag("abcdef"))
 	_, err = HandlePromoteEnvironment(kubeclient, radixclient, "anyapp3", PromotionParameters{FromEnvironment: "dev", ImageTag: "abcdef", ToEnvironment: "prod"})
 	assert.Error(t, err, "HandlePromoteEnvironment - Cannot promote an image into environment having already that image")
 
+	createNamespace(kubeclient, "anyapp4", "dev")
+	createNamespace(kubeclient, "anyapp4", "prod")
+	_, err = HandlePromoteEnvironment(kubeclient, radixclient, "anyapp4", PromotionParameters{FromEnvironment: "dev", ToEnvironment: "prod"})
+	assert.Error(t, err, "HandlePromoteEnvironment - Cannot promote non-existing image")
+	assert.Equal(t, "No latest deployment was found", (err.(*utils.Error)).Message, "HandlePromoteEnvironment - Unexpected message")
 }
 
-func save(kubeclient *kubernetes.Clientset, radixclient *radix.Clientset, builder DeploymentBuilder) {
+func TestPromote_HappyPathScenarios_NewStateIsExpected(t *testing.T) {
+	kubeclient := kubernetes.NewSimpleClientset()
+	radixclient := radix.NewSimpleClientset()
+
+	apply(kubeclient, radixclient, NewDeploymentBuilder().withAppName("anyapp").withEnvironment("dev").withImageTag("abcdef"))
+
+	// Create prod environment without any deployments
+	createNamespace(kubeclient, "anyapp", "prod")
+	_, err := HandlePromoteEnvironment(kubeclient, radixclient, "anyapp", PromotionParameters{FromEnvironment: "dev", ImageTag: "abcdef", ToEnvironment: "prod"})
+	assert.NoError(t, err, "HandlePromoteEnvironment - Unexpected error")
+
+	deployments, _ := HandleGetDeployments(radixclient, "anyapp", "prod", false)
+	assert.Equal(t, 1, len(deployments), "HandlePromoteEnvironment - Promoted as expected")
+
+	// Ensure the second image is considered the latest version
+	apply(kubeclient, radixclient, NewDeploymentBuilder().withAppName("anyapp1").withEnvironment("dev").withImageTag("ghijklm").withCreated(time.Now()))
+	apply(kubeclient, radixclient, NewDeploymentBuilder().withAppName("anyapp1").withEnvironment("dev").withImageTag("nopqrst").withCreated(time.Now().AddDate(0, 0, 1)))
+
+	createNamespace(kubeclient, "anyapp1", "prod")
+
+	// No image tag should promote latest
+	_, err = HandlePromoteEnvironment(kubeclient, radixclient, "anyapp1", PromotionParameters{FromEnvironment: "dev", ToEnvironment: "prod"})
+	assert.NoError(t, err, "HandlePromoteEnvironment - Unexpected error")
+
+	deployments, _ = HandleGetDeployments(radixclient, "anyapp1", "prod", false)
+	assert.Equal(t, 1, len(deployments), "HandlePromoteEnvironment - Promoted as expected")
+	assert.Equal(t, getDeploymentName("anyapp1", "nopqrst"), deployments[0].Name, "HandlePromoteEnvironment - Unexpected image promoted")
+}
+
+func apply(kubeclient *kubernetes.Clientset, radixclient *radix.Clientset, builder Builder) {
 	rd := builder.BuildRD()
 
-	ns := getNameSpaceForApplicationEnvironment(rd.Spec.AppName, rd.Spec.Environment)
+	ns := createNamespace(kubeclient, rd.Spec.AppName, rd.Spec.Environment)
+	radixclient.RadixV1().RadixDeployments(ns).Create(rd)
+}
+
+func createNamespace(kubeclient *kubernetes.Clientset, appName, environment string) string {
+	ns := getNamespaceForApplicationEnvironment(appName, environment)
 	namespace := corev1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: ns,
@@ -87,72 +131,5 @@ func save(kubeclient *kubernetes.Clientset, radixclient *radix.Clientset, builde
 	}
 
 	kubeclient.CoreV1().Namespaces().Create(&namespace)
-	radixclient.RadixV1().RadixDeployments(ns).Create(rd)
-}
-
-// TODO: Should this be a test functionality or moved into main code?
-// DeploymentBuilder Handles construction of RD
-type DeploymentBuilder interface {
-	withImageTag(string) DeploymentBuilder
-	withAppName(string) DeploymentBuilder
-	withEnvironment(string) DeploymentBuilder
-	withTime(time.Time) DeploymentBuilder
-	BuildRD() *v1.RadixDeployment
-}
-
-type deploymentBuilder struct {
-	imageTag    string
-	appName     string
-	environment string
-	time        time.Time
-}
-
-func (db *deploymentBuilder) withImageTag(imageTag string) DeploymentBuilder {
-	db.imageTag = imageTag
-	return db
-}
-
-func (db *deploymentBuilder) withAppName(appName string) DeploymentBuilder {
-	db.appName = appName
-	return db
-}
-
-func (db *deploymentBuilder) withEnvironment(environment string) DeploymentBuilder {
-	db.environment = environment
-	return db
-}
-
-func (db *deploymentBuilder) withTime(time time.Time) DeploymentBuilder {
-	db.time = time
-	return db
-}
-
-func (db *deploymentBuilder) BuildRD() *v1.RadixDeployment {
-	radixDeployment := &v1.RadixDeployment{
-		TypeMeta: metav1.TypeMeta{
-			APIVersion: "radix.equinor.com/v1",
-			Kind:       "RadixDeployment",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      getDeploymentName(db.appName, db.imageTag),
-			Namespace: getNameSpaceForApplicationEnvironment(db.appName, db.environment),
-			Labels: map[string]string{
-				"radixApp": db.appName,
-				"env":      db.environment,
-			},
-			CreationTimestamp: metav1.Time{Time: db.time},
-		},
-		Spec: v1.RadixDeploymentSpec{
-			AppName:     db.appName,
-			Environment: db.environment,
-		},
-	}
-	return radixDeployment
-}
-
-// NewDeploymentBuilder Constructor for deployment builder
-func NewDeploymentBuilder() DeploymentBuilder {
-	return &deploymentBuilder{
-		time: time.Now(),
-	}
+	return ns
 }
