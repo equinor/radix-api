ec5cba6140ffcefcb101dce04bf4b4ffd5919274
Dafferianto Trinugroho <daft@Dafferiantos-MacBook-Pro.local>
2018-10-23 15:10:57 +0200

    Pass commit ID from API server to pipeline

--- api/applications/pipeline_parameters.go
+++ api/applications/pipeline_parameters.go
@@ -1,6 +1,6 @@
 package applications
 
-// PipelineParameters describe branch to build
+// PipelineParameters describe branch to build and its commit ID
 // swagger:model PipelineParameters
 type PipelineParameters struct {
 	// Branch the branch to build
@@ -8,4 +8,10 @@ type PipelineParameters struct {
 	// required: true
 	// example: master
 	Branch string `json:"branch"`
+
+	// CommitID the commit ID of the branch to build
+	//
+	// required: true
+	// example: 4faca8595c5283a9d0f17a623b9255a0d9866a2e
+	CommitID string `json:"commitID"`
 }
