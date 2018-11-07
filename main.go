e55cb70652b7395f49630af211ba1171444dbd94
Dafferianto Trinugroho <daft@Dafferiantos-MacBook-Pro.local>
2018-11-07 09:39:27 +0100

    Bump Swagger version

--- main.go
+++ main.go
@@ -2,7 +2,7 @@
 // This is the API Server for the Radix platform.
 // Schemes: https, http
 // BasePath: /api/v1
-// Version: 0.0.37
+// Version: 0.0.38
 // Contact: https://equinor.slack.com/messages/CBKM6N2JY
 //
 // Consumes:
