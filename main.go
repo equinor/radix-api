448fbe92f264aab03772725ec9485815f1e20a92
ingeknudsen <iknu@equinor.com>
2018-11-19 14:16:53 +0100

    Renamed to latestJob as per spec

--- main.go
+++ main.go
@@ -2,7 +2,7 @@
 // This is the API Server for the Radix platform.
 // Schemes: https, http
 // BasePath: /api/v1
-// Version: 0.0.42
+// Version: 0.0.43
 // Contact: https://equinor.slack.com/messages/CBKM6N2JY
 //
 // Consumes:
