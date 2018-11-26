c81347e87d9cbf9fb037ca47dfcb5cb1acb2592b
Dafferianto Trinugroho <daft@statoil.com>
2018-11-26 15:33:44 +0100

    Bump up swagger version

--- main.go
+++ main.go
@@ -2,7 +2,7 @@
 // This is the API Server for the Radix platform.
 // Schemes: https, http
 // BasePath: /api/v1
-// Version: 0.0.46
+// Version: 0.0.47
 // Contact: https://equinor.slack.com/messages/CBKM6N2JY
 //
 // Consumes:
