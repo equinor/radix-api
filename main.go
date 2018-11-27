ef37002506b6eb024404b603c4aa8b1222bba123
ingeknudsen <iknu@equinor.com>
2018-11-27 08:15:17 +0100

    Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,7 @@
 // This is the API Server for the Radix platform.
 // Schemes: https, http
 // BasePath: /api/v1
-// Version: 0.0.49
+// Version: 0.0.50
 // Contact: https://equinor.slack.com/messages/CBKM6N2JY
 //
 // Consumes:
