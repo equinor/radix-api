f1a878011044ab097fa638dd9221b66fd8b3c9bb
ingeknudsen <iknu@equinor.com>
2018-12-20 11:54:10 +0100

    Attempt to avvoid panic + added test

--- api/jobs/job_controller_test.go
+++ api/jobs/job_controller_test.go
@@ -9,10 +9,13 @@ import (
 	. "github.com/statoil/radix-api/api/jobs"
 	jobModels "github.com/statoil/radix-api/api/jobs/models"
 	controllertest "github.com/statoil/radix-api/api/test"
+	"github.com/statoil/radix-operator/pkg/apis/kube"
 	commontest "github.com/statoil/radix-operator/pkg/apis/test"
 	builders "github.com/statoil/radix-operator/pkg/apis/utils"
 	radixclient "github.com/statoil/radix-operator/pkg/client/clientset/versioned"
 	"github.com/statoil/radix-operator/pkg/client/clientset/versioned/fake"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kubernetes "k8s.io/client-go/kubernetes"
 	kubefake "k8s.io/client-go/kubernetes/fake"
 )
@@ -52,20 +55,37 @@ func TestGetApplicationJob(t *testing.T) {
 		CommitID: anyPushCommitID,
 	}
 
-	// Test
-	t.Run("job started ok", func(t *testing.T) {
-		handler := Init(client, radixclient)
-		jobSummary, _ := handler.HandleStartPipelineJob(anyAppName, anyCloneURL, anyPipeline, jobParameters)
-		responseChannel := controllerTestUtils.ExecuteRequest("GET", fmt.Sprintf("/api/v1/applications/%s/jobs/%s", anyAppName, jobSummary.Name))
-		response := <-responseChannel
+	handler := Init(client, radixclient)
+	jobSummary, _ := handler.HandleStartPipelineJob(anyAppName, anyCloneURL, anyPipeline, jobParameters)
+	createPipelinePod(client, builders.GetAppNamespace(anyAppName), jobSummary.Name)
 
-		job := jobModels.Job{}
-		controllertest.GetResponseBody(response, &job)
-		assert.Equal(t, jobSummary.Name, job.Name)
-		assert.Equal(t, anyBranch, job.Branch)
-		assert.Equal(t, anyPushCommitID, job.CommitID)
-		assert.Equal(t, anyPipeline.String(), job.Pipeline)
-	})
+	// Test
+	responseChannel := controllerTestUtils.ExecuteRequest("GET", fmt.Sprintf("/api/v1/applications/%s/jobs/%s", anyAppName, jobSummary.Name))
+	response := <-responseChannel
+
+	job := jobModels.Job{}
+	controllertest.GetResponseBody(response, &job)
+	assert.Equal(t, jobSummary.Name, job.Name)
+	assert.Equal(t, anyBranch, job.Branch)
+	assert.Equal(t, anyPushCommitID, job.CommitID)
+	assert.Equal(t, anyPipeline.String(), job.Pipeline)
+	assert.Empty(t, job.Steps)
+
+	cloneStep := corev1.ContainerStatus{Name: "clone", State: corev1.ContainerState{Waiting: &corev1.ContainerStateWaiting{}}}
+	addStepToPipelinePod(client, builders.GetAppNamespace(anyAppName), jobSummary.Name, cloneStep)
+
+	responseChannel = controllerTestUtils.ExecuteRequest("GET", fmt.Sprintf("/api/v1/applications/%s/jobs/%s", anyAppName, jobSummary.Name))
+	response = <-responseChannel
+
+	job = jobModels.Job{}
+	controllertest.GetResponseBody(response, &job)
+	assert.Equal(t, jobSummary.Name, job.Name)
+	assert.Equal(t, anyBranch, job.Branch)
+	assert.Equal(t, anyPushCommitID, job.CommitID)
+	assert.Equal(t, anyPipeline.String(), job.Pipeline)
+	assert.NotEmpty(t, job.Steps)
+
+	assert.
 
 }
 
@@ -82,4 +102,32 @@ func TestGetPipelineJobLogsError(t *testing.T) {
 		assert.NotNil(t, err)
 		assert.Equal(t, pipelineNotFoundError.Error(), err.Error())
 	})
+}
+
+func createPipelinePod(kubeclient kubernetes.Interface, namespace, jobName string) {
+	podSpec := getPodSpecForAPipelineJob(jobName)
+	kubeclient.CoreV1().Pods(namespace).Create(podSpec)
+}
+
+func addStepToPipelinePod(kubeclient kubernetes.Interface, namespace, jobName string, jobStep corev1.ContainerStatus) {
+	pipelinePod, _ := kubeclient.CoreV1().Pods(namespace).Get(jobName, metav1.GetOptions{})
+	podStatus := corev1.PodStatus{
+		ContainerStatuses: []corev1.ContainerStatus{
+			jobStep,
+		},
+	}
+
+	pipelinePod.Status = podStatus
+	kubeclient.CoreV1().Pods(namespace).Update(pipelinePod)
+}
+
+func getPodSpecForAPipelineJob(jobName string) *corev1.Pod {
+	return &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: jobName,
+			Labels: map[string]string{
+				kube.RadixJobNameLabel: jobName,
+			},
+		},
+	}
 }
