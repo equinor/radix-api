1d8b97fe81330078b2bdc3044f8505b4ba13cb04
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2019-05-13 09:45:59 +0200

    Prepare for review

--- gitclone/docker-entrypoint.sh
+++ gitclone/docker-entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-# chmod 700 /root/.ssh
-# chmod 400 /root/.ssh/id_rsa
-
 SHA256_RSA_FINGERPRINT="SHA256:nThbg6kXUpJWGl7E1IGOCspRomTxdCARLviKw6E5SY8"
 KNOWN_HOSTS=/root/dynamic_known_host
-# ssh-keyscan github.com 2>/dev/null | tee keyscan.txt | cut -f 2,3 -d ' ' | ssh-keygen -lf - | cut -f 2 -d ' ' | grep -q "$SHA256_RSA_FINGERPRINT" && cat keyscan.txt >> $KNOWN_HOSTS && rm keyscan.txt || (echo FAIL && exit 1)
-ssh-keyscan -H github.com >> $KNOWN_HOSTS
+ssh-keyscan -H github.com 2>/dev/null | tee keyscan.txt | cut -f 2,3 -d ' ' | ssh-keygen -lf - | cut -f 2 -d ' ' | grep -q "$SHA256_RSA_FINGERPRINT" && cat keyscan.txt >> $KNOWN_HOSTS && rm keyscan.txt || (echo FAIL && exit 1)
 
 chmod 400 $KNOWN_HOSTS
 /bin/sh -c bash
--- gitclone/gitclone.Dockerfile
+++ gitclone/gitclone.Dockerfile
@@ -1,8 +1,4 @@
-FROM alpine:3.7
-
-# TODO: Remove this when done
-# RUN mkdir /root/.ssh
-# COPY id_rsa /root/.ssh/id_rsa
+FROM alpine:3.9
 
 RUN apk add --no-cache bash openssh-client git
 ENV GIT_SSH_COMMAND="ssh -i /root/.ssh/id_rsa -o UserKnownHostsFile=/root/dynamic_known_host"
@@ -14,4 +10,7 @@ RUN mkdir /root/bin
 COPY docker-entrypoint.sh /root/bin/docker-entrypoint.sh
 RUN chmod +x /root/bin/docker-entrypoint.sh
 
-ENTRYPOINT [ "/root/bin/docker-entrypoint.sh"]
+RUN sh /root/bin/docker-entrypoint.sh
+RUN chmod +r /root/dynamic_known_host
+
+CMD ["/bin/sh", "-c"]
