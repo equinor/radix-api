403247da60663a5ee8f93fe224d4e4b91c9af862
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2019-08-08 15:37:00 +0200

    Strategic patch does not work for CRD

--- api/jobs/stop_job_handler.go
+++ api/jobs/stop_job_handler.go
@@ -1,17 +1,13 @@
 package jobs
 
 import (
-	"encoding/json"
 	"fmt"
 
 	jobModels "github.com/equinor/radix-api/api/jobs/models"
-	v1 "github.com/equinor/radix-operator/pkg/apis/radix/v1"
 	crdUtils "github.com/equinor/radix-operator/pkg/apis/utils"
 	log "github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/apimachinery/pkg/util/strategicpatch"
 )
 
 // StopJob Stops an application job
@@ -27,25 +23,9 @@ func (jh JobHandler) StopJob(appName, jobName string) error {
 		return err
 	}
 
-	newJob := job.DeepCopy()
-	newJob.Spec.Stop = true
+	job.Spec.Stop = true
 
-	jobJSON, err := json.Marshal(job)
-	if err != nil {
-		return fmt.Errorf("Failed to marshal old job object: %v", err)
-	}
-
-	newJobJSON, err := json.Marshal(newJob)
-	if err != nil {
-		return fmt.Errorf("Failed to marshal new job object: %v", err)
-	}
-
-	patchBytes, err := strategicpatch.CreateTwoWayMergePatch(jobJSON, newJobJSON, v1.RadixJob{})
-	if err != nil {
-		return fmt.Errorf("Failed to create two way merge patch job objects: %v", err)
-	}
-
-	_, err = jh.userAccount.RadixClient.RadixV1().RadixJobs(appNamespace).Patch(jobName, types.StrategicMergePatchType, patchBytes)
+	_, err = jh.userAccount.RadixClient.RadixV1().RadixJobs(appNamespace).Update(job)
 	if err != nil {
 		return fmt.Errorf("Failed to patch job object: %v", err)
 	}
