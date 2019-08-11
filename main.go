8ab2e6a574404e3fc8f977d45692262c172e5554
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2019-08-11 11:53:49 +0200

    Increase API version

--- main.go
+++ main.go
@@ -2,7 +2,7 @@
 // This is the API Server for the Radix platform.
 // Schemes: https, http
 // BasePath: /api/v1
-// Version: 0.0.92
+// Version: 0.0.93
 // Contact: https://equinor.slack.com/messages/CBKM6N2JY
 //
 // Consumes:
