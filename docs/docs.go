bd264c896784566135881ea4e406d2ce39ce397b
ingeknudsen <iknu@equinor.com>
2020-03-19 07:11:26 +0100

    Ra 1315   improve logging (#246)
    * Allow for filtering logs by timestamp
    
    * New swagger version
    
    Co-authored-by: INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>

--- docs/docs.go
+++ docs/docs.go
@@ -4,7 +4,7 @@
 //
 //     Schemes: http, https
 //     BasePath: /api/v1
-//     Version: 1.3.6
+//     Version: 1.3.7
 //     Contact: https://equinor.slack.com/messages/CBKM6N2JY
 //
 //     Consumes:
