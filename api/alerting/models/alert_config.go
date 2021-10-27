a9f32fc5ad11f767cbed12aa36962ce2e15105ca
Nils Gustav Stråbø <nst@equinor.com>
2021-10-27 14:40:09 +0200

    align method name

--- api/alerting/models/alert_config.go
+++ api/alerting/models/alert_config.go
@@ -5,7 +5,7 @@ import radixv1 "github.com/equinor/radix-operator/pkg/apis/radix/v1"
 //AlertConfigList list of AlertConfig
 type AlertConfigList []AlertConfig
 
-// AsRadixAlertAlertsList converts list of AlertConfigs to list of alerts to be used in RadixAlert spec
+// AsRadixAlertAlerts converts list of AlertConfigs to list of alerts to be used in RadixAlert spec
 func (l AlertConfigList) AsRadixAlertAlerts() []radixv1.Alert {
 	var alerts []radixv1.Alert
 
