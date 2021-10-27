c4c7838709b911440766e04eaaef72f99a4c5476
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2021-10-27 15:47:16 +0200

    add alerting handler for environment and application (#361)
    * configure alerting for environments
    
    * update reference to radix-operator
    
    * add environment handler wrapper func
    
    * add alerting routes for application
    
    * set json name annotation
    
    * remove alert name lister
    
    * return empty alertingconfig when disabling alerting
    
    * make slackconfig nilable
    
    * fix incorrect error handling
    
    * tests and recfactor
    
    * alerting handler unit tests
    
    * alerting handler unit tests
    
    * alerrting model unit test
    
    * update unit test
    
    * rename
    
    * update version
    
    * align method name
    
    * move alerting to new controller
    
    * fix naming
    
    * fix swagger definition
    
    Co-authored-by: Nils Gustav Stråbø <nst@equinor.com>

--- docs/docs.go
+++ docs/docs.go
@@ -4,7 +4,7 @@
 //
 //     Schemes: http, https
 //     BasePath: /api/v1
-//     Version: 1.15.1
+//     Version: 1.16.0
 //     Contact: https://equinor.slack.com/messages/CBKM6N2JY
 //
 //     Consumes:
