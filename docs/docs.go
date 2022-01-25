89d02a4daf855b49159993f79aaeeedf48724bc5
Sergey Smolnikov <ssmol@equinor.com>
2022-01-25 16:45:31 +0100

    Fixed key-vault secrets (#376)

--- docs/docs.go
+++ docs/docs.go
@@ -4,7 +4,7 @@
 //
 //     Schemes: http, https
 //     BasePath: /api/v1
-//     Version: 1.18.1
+//     Version: 1.18.2
 //     Contact: https://equinor.slack.com/messages/CBKM6N2JY
 //
 //     Consumes:
