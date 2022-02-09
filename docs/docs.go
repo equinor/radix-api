936e08e4c6fb72c487ac32cd0ee36fa9a60bfa86
Nils Gustav Stråbø <nst@equinor.com>
2022-02-09 15:19:31 +0100

    update version

--- docs/docs.go
+++ docs/docs.go
@@ -4,7 +4,7 @@
 //
 //     Schemes: http, https
 //     BasePath: /api/v1
-//     Version: 1.18.3
+//     Version: 1.19.0
 //     Contact: https://equinor.slack.com/messages/CBKM6N2JY
 //
 //     Consumes:
