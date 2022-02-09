3491953e79402fa50048c6090abc7984f7cc23b6
Nils Gustav Stråbø <nst@equinor.com>
2022-02-09 10:22:44 +0100

    handle logs from non-ready container

--- api/pods/pod_handler.go
+++ api/pods/pod_handler.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/equinor/radix-api/api/utils/labelselector"
@@ -87,6 +88,9 @@ func (ph PodHandler) getPodLogFor(pod *corev1.Pod, containerName string, sinceTi
 	req := getPodLogRequest(ph.client, pod, containerName, false, sinceTime)
 	readCloser, err := req.Stream(context.TODO())
 	if err != nil {
+		if errors.IsBadRequest(err) {
+			return "", nil
+		}
 		return "", err
 	}
 
