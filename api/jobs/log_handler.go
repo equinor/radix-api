e22151e535efffc74abc0845fa9e333983467681
Sergey Smolnikov <ssmol@equinor.com>
2022-06-29 16:10:50 +0200

    Removed outdated method

--- api/jobs/log_handler.go
+++ api/jobs/log_handler.go
@@ -1,7 +1,6 @@
 package jobs
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	v1 "github.com/equinor/radix-operator/pkg/apis/radix/v1"
@@ -17,28 +16,7 @@ import (
 	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
 )
-
-// GetApplicationJobLogs Gets logs for a job of an application
-func (jh JobHandler) GetApplicationJobLogs(appName, jobName string, sinceTime *time.Time, logLines *int64) ([]jobModels.StepLog, error) {
-	job, err := jh.userAccount.RadixClient.RadixV1().RadixJobs(crdUtils.GetAppNamespace(appName)).Get(context.TODO(), jobName, metav1.GetOptions{})
-	if errors.IsNotFound(err) {
-		return nil, jobModels.PipelineNotFoundError(appName, jobName)
-	}
-	if err != nil {
-		return nil, err
-	}
-
-	steps := jobModels.GetJobStepsFromRadixJob(job)
-
-	var logs []jobModels.StepLog
-	for _, step := range steps {
-		stepLog := getStepLog(jh.userAccount.Client, appName, step, sinceTime, logLines)
-		logs = append(logs, stepLog)
-	}
-	return logs, nil
-}
 
 // GetTektonPipelineRunTaskStepLogs Get logs of a pipeline run task for a pipeline job
 func (jh JobHandler) GetTektonPipelineRunTaskStepLogs(appName, jobName, pipelineRunName, taskName, stepName string, sinceTime *time.Time, logLines *int64) (io.ReadCloser, error) {
@@ -118,25 +96,4 @@ func getPodNameForStep(job *v1.RadixJob, stepName string) string {
 		}
 	}
 	return ""
-}
-
-func getStepLog(client kubernetes.Interface, appName string, step jobModels.Step, sinceTime *time.Time, logLines *int64) jobModels.StepLog {
-	var buildLog string
-	podHandler := pods.Init(client)
-	logReader, err := podHandler.HandleGetAppPodLog(appName, step.PodName, step.Name, sinceTime, logLines)
-
-	if err != nil {
-		log.Warnf("Failed to get build logs. %v", err)
-		buildLog = fmt.Sprintf("%v", err)
-	} else {
-		var buf bytes.Buffer
-		_, _ = buf.ReadFrom(logReader)
-		buildLog = buf.String()
-	}
-
-	return jobModels.StepLog{
-		Name:    step.Name,
-		Log:     buildLog,
-		PodName: step.PodName,
-	}
 }
