2ec9f04bd71a8a0d27af7bf7ee5fc31631d6d816
Nils Gustav Stråbø <nst@equinor.com>
2023-01-03 13:20:30 +0100

    add namespace field to Deployment

--- api/deployments/deployment_handler.go
+++ api/deployments/deployment_handler.go
@@ -10,7 +10,6 @@ import (
 	deploymentModels "github.com/equinor/radix-api/api/deployments/models"
 	"github.com/equinor/radix-api/api/pods"
 	"github.com/equinor/radix-api/models"
-	radixutils "github.com/equinor/radix-common/utils"
 	"github.com/equinor/radix-operator/pkg/apis/kube"
 	v1 "github.com/equinor/radix-operator/pkg/apis/radix/v1"
 	operatorUtils "github.com/equinor/radix-operator/pkg/apis/utils"
@@ -134,14 +133,6 @@ func (deploy *deployHandler) GetDeploymentWithName(appName, deploymentName strin
 		return nil, err
 	}
 
-	var activeTo time.Time
-	if !strings.EqualFold(deploymentSummary.ActiveTo, "") {
-		activeTo, err = radixutils.ParseTimestamp(deploymentSummary.ActiveTo)
-		if err != nil {
-			return nil, err
-		}
-	}
-
 	components, err := deploy.GetComponentsForDeployment(appName, deploymentSummary)
 	if err != nil {
 		return nil, err
@@ -154,8 +145,7 @@ func (deploy *deployHandler) GetDeploymentWithName(appName, deploymentName strin
 	}
 
 	dep, _ := deploymentModels.NewDeploymentBuilder().
-		WithRadixDeployment(*rd).
-		WithActiveTo(activeTo).
+		WithRadixDeployment(rd).
 		WithComponents(components).
 		WithGitCommitHash(rd.Annotations[kube.RadixCommitAnnotation]).
 		WithGitTags(rd.Annotations[kube.RadixGitTagsAnnotation]).
@@ -247,7 +237,7 @@ func (deploy *deployHandler) getDeployments(namespace, appName, jobName string, 
 
 		deploySummary, err := deploymentModels.
 			NewDeploymentBuilder().
-			WithRadixDeployment(rd).
+			WithRadixDeployment(&rd).
 			WithPipelineJob(radixJobMap[rd.Labels[kube.RadixJobNameLabel]]).
 			WithRadixRegistration(rr).
 			BuildDeploymentSummary()
