762323e5fdda01d75b63fe1fb4b17e5629ad313e
Sergey Smolnikov <ssmol@equinor.com>
2023-01-13 09:09:57 +0100

    Do not fail on creating a pipeline job in radix-api on unmatching branch (#459)

--- api/applications/applications_handler.go
+++ api/applications/applications_handler.go
@@ -525,7 +525,7 @@ func (ah *ApplicationHandler) triggerPipelineBuildOrBuildDeploy(appName, pipelin
 		return nil, applicationModels.AppNameAndBranchAreRequiredForStartingPipeline()
 	}
 
-	log.Infof("Creating build pipeline job for %s on branch %s for commit %s", appName, branch, commitID)
+	log.Infof("Requested build pipeline job for %s on branch %s for commit %s", appName, branch, commitID)
 
 	radixRegistration, err := ah.getServiceAccount().RadixClient.RadixV1().RadixRegistrations().Get(context.TODO(), appName, metav1.GetOptions{})
 	if err != nil {
@@ -539,9 +539,9 @@ func (ah *ApplicationHandler) triggerPipelineBuildOrBuildDeploy(appName, pipelin
 			return nil, err
 		}
 		isThereAnythingToDeploy, _ := application.IsThereAnythingToDeploy(branch)
-
 		if !isThereAnythingToDeploy {
-			return nil, applicationModels.UnmatchedBranchToEnvironment(branch)
+			log.Infof("There is environments for %s, matching to the branch %s. Skip the pipeline job creation", appName, branch)
+			return nil, nil
 		}
 	}
 
@@ -551,6 +551,8 @@ func (ah *ApplicationHandler) triggerPipelineBuildOrBuildDeploy(appName, pipelin
 	if err != nil {
 		return nil, err
 	}
+
+	log.Infof("Creating build pipeline job for %s on branch %s for commit %s", appName, branch, commitID)
 
 	jobSummary, err := ah.jobHandler.HandleStartPipelineJob(appName, pipeline, jobParameters)
 	if err != nil {
