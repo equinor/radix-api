33bc195f6ebf7960c7c94ac5394bdc6700c5854c
Richard Hagen <richard.hagen@bouvet.no>
2023-12-11 12:57:11 +0100

    add warning on breaking change to api

--- swaggerui/swagger.go
+++ swaggerui/swagger.go
@@ -3,24 +3,6 @@ package swaggerui
 import (
 	"embed"
 	"io/fs"
-	// Required to include swagger dependencies
-	// _ "github.com/Masterminds/sprig/v3"
-	// _ "github.com/go-openapi/analysis"
-	// _ "github.com/go-openapi/errors"
-	// _ "github.com/go-openapi/inflect"
-	// _ "github.com/go-openapi/loads"
-	// _ "github.com/go-openapi/loads/fmts"
-	// _ "github.com/go-openapi/runtime"
-	// _ "github.com/go-openapi/spec"
-	// _ "github.com/go-openapi/strfmt"
-	// _ "github.com/go-openapi/validate"
-	// _ "github.com/go-swagger/go-swagger"
-	// _ "github.com/jessevdk/go-flags"
-	// _ "github.com/toqueteos/webbrowser"
-	//
-	// _ "github.com/go-swagger/go-swagger/cmd/swagger/commands"
-	// _ "github.com/go-swagger/go-swagger/codescan"
-	// _ "github.com/go-swagger/go-swagger/generator"
 
 	_ "github.com/go-swagger/go-swagger"
 )
