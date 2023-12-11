5fe3a8413645a6e23a389ef8debd7f699aee1444
Richard Hagen <richard.hagen@bouvet.no>
2023-12-11 09:59:39 +0100

    install swagger

--- docs/docs.go
+++ docs/docs.go
@@ -2,7 +2,7 @@
 //
 // This is the API Server for the Radix platform.
 //
-//	Schemes: https
+//	Schemes: https, http
 //	BasePath: /api/v1
 //	Version: 1.23.0
 //	Contact: https://equinor.slack.com/messages/CBKM6N2JY
