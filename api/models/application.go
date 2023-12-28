6386b852ecda4e2355c9bccfa342ab0c75678f21
Sergey Smolnikov <ssmol@equinor.com>
2023-12-28 11:51:36 +0100

    Get only existing DNS aliases (#577)
    * Get only existing DNS aliases
    
    * Removed outdated argument

--- api/models/application.go
+++ api/models/application.go
@@ -7,17 +7,17 @@ import (
 )
 
 // BuildApplication builds an Application model.
-func BuildApplication(rr *radixv1.RadixRegistration, ra *radixv1.RadixApplication, reList []radixv1.RadixEnvironment, rdList []radixv1.RadixDeployment, rjList []radixv1.RadixJob, ingressList []networkingv1.Ingress, userIsAdmin bool, radixDNSAliasList []radixv1.RadixDNSAlias, radixDNSZone string) *applicationModels.Application {
+func BuildApplication(rr *radixv1.RadixRegistration, ra *radixv1.RadixApplication, reList []radixv1.RadixEnvironment, rdList []radixv1.RadixDeployment, rjList []radixv1.RadixJob, ingressList []networkingv1.Ingress, userIsAdmin bool, dnsAliases []applicationModels.DNSAlias) *applicationModels.Application {
 	application := applicationModels.Application{
 		Name:         rr.Name,
 		Registration: *BuildApplicationRegistration(rr),
 		Jobs:         BuildJobSummaryList(rjList),
 		AppAlias:     BuildApplicationAlias(ingressList, reList),
 		UserIsAdmin:  userIsAdmin,
+		DNSAliases:   dnsAliases,
 	}
 	if ra != nil {
 		application.Environments = BuildEnvironmentSummaryList(rr, ra, reList, rdList, rjList)
-		application.DNSAliases = BuildDNSAlias(ra, radixDNSAliasList, radixDNSZone)
 	}
 	return &application
 }
