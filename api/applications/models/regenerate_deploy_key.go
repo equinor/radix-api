cf511d9085cb44d81167cec0248c5b8a8a1a7658
Sergey Smolnikov <ssmol@equinor.com>
2025-05-28 13:57:53 +0200

    Removed outdated shared key property

--- api/applications/models/regenerate_deploy_key.go
+++ api/applications/models/regenerate_deploy_key.go
@@ -3,12 +3,6 @@ package models
 // RegenerateDeployKeyData Holds regenerated shared secret
 // swagger:model RegenerateDeployKeyData
 type RegenerateDeployKeyData struct {
-	// Deprecated: use RegenerateSharedSecretData instead
-	// SharedSecret of the shared secret
-	//
-	// required: false
-	SharedSecret string `json:"sharedSecret"`
-
 	// PrivateKey of the deploy key
 	//
 	// required: false
