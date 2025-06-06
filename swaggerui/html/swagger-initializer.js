1c822d1d4f39c33cbb7b4d5fa9debf6aa804706d
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2025-06-06 15:49:53 +0200

    chore: bump swaggerui to v5.24.0

--- swaggerui/html/swagger-initializer.js
+++ swaggerui/html/swagger-initializer.js
@@ -13,7 +13,7 @@ window.onload = function() {
     plugins: [
       SwaggerUIBundle.plugins.DownloadUrl
     ],
-    layout: "StandaloneLayout"
+    layout: "StandaloneLayout",
   });
 
   //</editor-fold>
